44ab4c4ec9ee5b55ba68d7b097905b3789cb9561
Maks <max.maisak@gmail.com>
2018-04-16 05:58:04 +0200

    Added bullet destruction when too far from origin

---
+++ Assets/Scripts/Utility/DestroyWhenOutOfBounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// Destroys its gameobject when it gets too far from origin 
+public class DestroyWhenOutOfBounds : MonoBehaviour {
+
+    [SerializeField] float maxDistance = 100f;
+    
+	void FixedUpdate() {
+
+        Vector3 position = transform.position;
+        if (
+            Mathf.Abs(position.x) > maxDistance ||
+            Mathf.Abs(position.y) > maxDistance ||
+            Mathf.Abs(position.z) > maxDistance
+        ) {
+            Destroy(gameObject);
+        }
+	}
+}
